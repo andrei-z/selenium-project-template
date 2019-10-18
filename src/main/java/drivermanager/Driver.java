be0f1a306f7167872083bb030409aa334f839ad5
andrei-z <andrei.zhulin@gmail.com>
2019-10-18 11:32:11 +0300

    Initial commit

---
+++ src/main/java/drivermanager/Driver.java
@@ -0,0 +1,139 @@
+package drivermanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class Driver implements WebDriver {
+
+    private WebDriver driver;
+    private String browserName;
+
+    public enum Browser {
+        CHROME("chrome"),
+        FIREFOX("firefox"),
+        SAFARI("safari"),
+        HTMLUNIT("htmlunit");
+
+        private String browser;
+
+        Browser(String browser){
+            this.browser = browser;
+        }
+
+        public String getBrowser(){
+            return browser;
+        }
+    }
+
+    public Driver(String browserName){
+        this.browserName = browserName;
+
+        if(browserName.equalsIgnoreCase(String.valueOf(Browser.CHROME)))
+            this.driver = new ChromeDriver();
+
+        if(browserName.equalsIgnoreCase(String.valueOf(Browser.FIREFOX)))
+            this.driver = new FirefoxDriver();
+
+        if(browserName.equalsIgnoreCase(String.valueOf(Browser.SAFARI)))
+            this.driver = new SafariDriver();
+
+        if(browserName.equalsIgnoreCase(String.valueOf(Browser.HTMLUNIT)))
+            this.driver = new HtmlUnitDriver();
+    }
+
+    public static String selectBrowser(Browser browser){
+        return browser.getBrowser();
+    }
+
+    public static String getParameter(String name){
+        String value = System.getProperty(name);
+        if (value == null || value.isEmpty())
+            value = "chrome"; // default browser if -Dbrowser is not defined, or the value does not exist
+
+        int browserFound = 0;
+        for(Browser b : Browser.values()){
+            if (value.equals(b.getBrowser())){
+                browserFound++;
+            }
+        }
+            if (browserFound!=1){
+                value = "chrome";
+                System.out.println("Failed to select the browser. Switching to Google Chrome...");
+            }
+
+        return value;
+    }
+
+    @Override
+    public void get(String url) {
+        this.driver.get(url);
+    }
+
+    @Override
+    public String getCurrentUrl() {
+        return this.driver.getCurrentUrl();
+    }
+
+    @Override
+    public String getTitle() {
+        return this.driver.getTitle();
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return this.driver.findElements(by);
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return this.driver.findElement(by);
+    }
+
+    @Override
+    public String getPageSource() {
+        return this.driver.getPageSource();
+    }
+
+    @Override
+    public void close() {
+        this.driver.close();
+    }
+
+    @Override
+    public void quit() {
+        this.driver.quit();
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        return this.driver.getWindowHandles();
+    }
+
+    @Override
+    public String getWindowHandle() {
+        return this.driver.getWindowHandle();
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        return this.driver.switchTo();
+    }
+
+    @Override
+    public Navigation navigate() {
+        return this.driver.navigate();
+    }
+
+    @Override
+    public Options manage() {
+        return this.driver.manage();
+    }
+}
---
+++ src/test/java/tests/ExamplesTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+import drivermanager.Driver;
+
+import static drivermanager.Driver.Browser.FIREFOX;
+import static drivermanager.Driver.getParameter;
+import static drivermanager.Driver.selectBrowser;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.BeforeClass;
+
+public class ExamplesTest {
+
+    private static Driver driver;
+
+    @BeforeClass
+    public static void setUp(){
+
+        String browserName = getParameter("browser"); // sets name to conjunct with -D (define a system property) Maven CLI option, results in -Dbrowser in this case
+        driver = new Driver(browserName);
+
+/*      The following is generally not recommended. But might be helpful for cross-browser tests preparation or debugging.
+ *      To select a browser from inside of the class, uncomment these 2 lines
+ *      (this should also work from inside of any particular @Test body as well):
+*/
+//      driver.quit(); // tear down browser instance invoked either by Maven or IDE
+//      driver = new Driver(selectBrowser(FIREFOX));
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    /* exampleOne [recommended]. @Test will run with the default 'chrome' browser both from IDE and Maven
+    *  unless a different browser is specified by maven in -Dbrowser or the browser is selected in @Before / @BeforeClass in IDE
+    * */
+    @Test
+    public void exampleOne(){
+        driver.get("http://yr.no/");
+        Assert.assertTrue(driver.getTitle().startsWith("Yr"));
+    }
+
+    /* exampleTwo [not recommended]. @Test will run with 'firefox' regardless of the run being started from IDE or Maven
+    * Browser is selected from inside of the @Test body
+    * */
+    @Test
+    public void exampleTwo(){
+        driver.quit();
+        driver = new Driver(selectBrowser(FIREFOX));
+
+        driver.get("http://yr.no/");
+        Assert.assertTrue(driver.getTitle().startsWith("Yr"));
+    }
+}
